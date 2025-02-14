5c19dfff6eb7f25ce75fb8bc036aacf2e18c3ae3
Naoki Watanabe <asterisk37n@gmail.com>
2025-02-14 17:06:50 +0900

    Update TimeSeriesMap component to use relative paths for marker icons

--- src/components/TimeSeriesMap.tsx
+++ src/components/TimeSeriesMap.tsx
@@ -14,14 +14,11 @@ import {
 } from 'date-fns';
 
 import L from 'leaflet';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 L.Icon.Default.mergeOptions({
-  iconUrl: markerIcon,
-  iconRetinaUrl: markerIcon2x,
-  shadowUrl: markerShadow,
+  iconUrl: '/marker-icon.png',
+  iconRetinaUrl: '/marker-icon-2x.png',
+  shadowUrl: '/marker-shadow.png',
 });
 
 const TimeSeriesMap = () => {
